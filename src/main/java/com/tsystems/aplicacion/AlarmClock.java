424cab34140254a9491dda6392145bfd6d130dcb
David Vaquero <pepesan@gmail.com>
2015-07-16 10:18:46 +0200

    propiedades métodos y sobrecarga

---
+++ src/main/java/com/tsystems/aplicacion/AlarmClock.java
@@ -0,0 +1,24 @@
+package com.tsystems.aplicacion;
+
+/**
+ * Created by pepesan on 16/7/15.
+ */
+public class AlarmClock {
+    // declare and initialize an instance variable
+    private  int snoozeInterval = 5;
+
+    public int getSnoozeInterval() {
+        return snoozeInterval;
+        // Must return an int
+        // Return the instance var's value
+    }
+    public void setSnoozeInterval(int snoozeInterval) {
+        this.snoozeInterval=snoozeInterval;
+    }
+    public void setSnoozeInterval(int hour, int minute) {
+        snoozeInterval=(60*hour)+minute;
+    }
+        public void snooze() {
+        System.out.println("ZZZZZ");
+    }
+}
--- src/main/java/com/tsystems/aplicacion/App.java
+++ src/main/java/com/tsystems/aplicacion/App.java
@@ -8,7 +8,37 @@ public class App
 {
     public static void main( String[] args )
     {
-        
-        System.out.println( "Hola Mundo!" );
+
+        //System.out.println( "Hola Mundo!" );
+
+        //Instancia de un objeto de la clase AlarmClock
+        AlarmClock reloj=new AlarmClock();
+        reloj.snooze();
+        System.out.println(reloj.getSnoozeInterval());
+        int snoozeInterval = reloj.getSnoozeInterval();
+
+        int n;// no initialization
+        int j = 0;// with initialization
+        int k = j + 1; // initialize with expression
+        float r = 0.0F;// float (decimals default to double)
+        long largo=1111_1111_1111L;
+        String hi = "Hello";
+
+        //Casting
+        float diameter = 6.77F;
+        float pi = 3.1415927F;
+        double circum = pi * diameter; // auto "upcast" to double
+        float approx = (float) circum; // explicit "downcast"
+        System.out.println(approx);
+        //call method
+        reloj.setSnoozeInterval(1000);
+        System.out.println(reloj.getSnoozeInterval());
+        reloj.setSnoozeInterval(1, 30);
+        System.out.println(reloj.getSnoozeInterval());
+
+        // call println(String)
+        System.out.println("2 + 2 = ");
+//      //call println(int)
+        System.out.println(4);
     }
 }
